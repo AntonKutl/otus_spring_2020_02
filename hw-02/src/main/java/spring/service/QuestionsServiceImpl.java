785852198438e592f88b91a9d8eeefedbd60d1cc
Anton Kutlubaev <write_me@live.ru>
2020-03-16 21:42:08 +0300

    -

--- hw-02/src/main/java/spring/service/QuestionsServiceImpl.java
+++ hw-02/src/main/java/spring/service/QuestionsServiceImpl.java
@@ -46,6 +46,7 @@ public class QuestionsServiceImpl implements QuestionsService {
                 counter++;
             }
         }
+
         System.out.println(name + " " + surname + messageSource.getMessage("question.answers",new Integer[]{counter}, Locale.forLanguageTag(local)));
     }
 }
