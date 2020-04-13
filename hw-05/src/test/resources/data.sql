ad6ef9d687f8d93c3828e84d7d223bbc8b8f9b62
AntonKutl <33842960+AntonKutl@users.noreply.github.com>
2020-04-13 15:44:56 +0300

    Delete data.sql

--- hw-05/src/test/resources/data.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT INTO genres (nameGenre) VALUES ('Роман');
-INSERT INTO authors(genresId, nameAuthor) VALUES (LAST_INSERT_ID(),'Толстой Л.Н');
-INSERT INTO books (authorsId, nameBook) VALUES (LAST_INSERT_ID(), 'Война и мир');
-
-
-
-
-
