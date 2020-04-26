98c5e01180d4150252f5534e663582959e14a718
AntonKutl <33842960+AntonKutl@users.noreply.github.com>
2020-04-26 19:18:47 +0300

    Delete CommentRepository.java

--- hw-07/src/main/java/otus/dao/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package otus.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import otus.model.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-}
