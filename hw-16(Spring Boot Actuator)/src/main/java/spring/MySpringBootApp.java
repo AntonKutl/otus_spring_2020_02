d384550fbe7123ebd5d480cd11ce9064b7faca77
Антон <write_me@live.ru>
2021-06-20 18:07:22 +0300

    Revert "."
    This reverts commit 35ae77a5

--- hw-16(Spring Boot Actuator)/src/main/java/spring/MySpringBootApp.java
+++ hw-16(Spring Boot Actuator)/src/main/java/spring/MySpringBootApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringBootApp {
 
-
     public static void main(String[] args) {
         SpringApplication.run(MySpringBootApp.class, args);
     }
